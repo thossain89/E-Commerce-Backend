89f94039457e1889549d5827bc8a2db30aad66c2
thossain <tanvirhossain2006@gmail.com>
2021-08-28 14:56:41 +1000

    product model updated

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,48 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    
+    },
+    product_name: {
+      type: DataTypes.STRING(150),
+      allowNull: false,
+      unique: true,
+      validate: {
+        isAlpha: true,
+      }
+    },
+    price : {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      }
+    },
+    stock :{
+      type : DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isInt: true,
+      }
+    },
+    category_id :{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'category',
+        key:'id',
+      },
+      validate: {
+        isInt: true,
+      }
+
+    },    
   },
   {
     sequelize,
